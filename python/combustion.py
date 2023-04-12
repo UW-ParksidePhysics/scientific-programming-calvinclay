fe29fc8090441d95b6e0925e02e832a3aebcb787
williamdparker <parkerw@uwp.edu>
2023-04-12 13:27:46 -0500

    Added second scrape and dictionary organization

--- python/combustion.py
+++ python/combustion.py
@@ -5,11 +5,34 @@ def scrape_web_data(url):
     return url_data
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
     gas_data_url = 'https://www.ohio.edu/mechanical/thermo/property_tables/gas/idealGas.html'
+    gas_data = scrape_web_data(gas_data_url)[0]
+    gas_names = gas_data[0]
+    gas_ratios = gas_data[6]
+    desired_gases = ['Hydrogen', 'Methane', 'Octane', 'Butane']
+    gas_data_dictionary = {}
+    for desired_gas in desired_gases:
+        for index, row in gas_data.iterrows():
+            if row[0] == desired_gas:
+                gas_data_dictionary[row[0]] = {
+                    'isochoric specific heat': row[5],
+                    'specific heat ratio': row[6]
+                }
+
+    combustion_data_url = 'https://en.wikipedia.org/wiki/Heat_of_combustion'
+    combustion_data = scrape_web_data(combustion_data_url)[3]
+    for desired_gas in desired_gases:
+        for index, row in combustion_data.iterrows():
+            if row[0] == desired_gas:
+                gas_data_dictionary[row[0]]['heat of combustion'] = row[4]
+
+    print(gas_data_dictionary)
+
+    # volume range to plot over -- use np.linspace()
+    # define P(V) = k / V^γ + P_0
+    # calculate ΔT at fixed V
+    # define P2(T1, T2, P1)
+
+
+
