167a9ae4f4a26c29156995e38d34f27738e9b623
clay0009 <clay0009@rangers.uwp.edu>
2023-04-26 12:25:40 -0500

    Office hour progress

--- python/combustion.py
+++ python/combustion.py
@@ -50,27 +50,33 @@ if __name__ == '__main__':
     # define P2
 # Hydrogen
 γ = 1.405
+# Bore of engine = 99.5mm, stroke of engine = 79mm
 open_volume = np.pi * (1 / 2 * 0.0995) ** 2 * (0.079)
 compressed_volume = 0.000100
-isobaric_heat_capacity = 517.15  # 244 degrees celsius maybe, have to look at data
-# heat of combustion?
-isochoric_heat_capacity = 283.333
+heat_of_combustion = 141.80e6
+isochoric_specific_heat = 10.183e3 #J/kgK
+fuel_density=0.08988 #kg/m3
+
+fuel_to_air_ratio= 1/14.7
 initial_temperature = 294
 initial_pressure = 101.3 * 10 ** 3  # atmospheric pressure
+fuel_mass = fuel_to_air_ratio * open_volume * fuel_density
 adiabatic_constant_1_2 = initial_pressure * open_volume ** γ
-temperature_change = (isobaric_heat_capacity / isochoric_heat_capacity) * 1000
+temperature_change = (heat_of_combustion / isochoric_specific_heat)
 raised_temperature = initial_temperature + temperature_change
+heat_in= fuel_mass * heat_of_combustion
 
-
+'''It will calculate the pressure for gas.'''
 def calculate_pressure(volume, k, initial_pressure):
-    return (k / (volume ** γ)) + initial_pressure
+    return (k / (volume ** γ)) #+ initial_pressure
 
 
 adiabatic_raised_pressure = calculate_pressure(compressed_volume, adiabatic_constant_1_2, initial_pressure)
-adiabatic_constant_3_4 = adiabatic_raised_pressure * open_volume ** γ
-adiabatic_lowered_pressure = calculate_pressure(open_volume, adiabatic_constant_3_4, adiabatic_raised_pressure)
-isochoric_raised_pressure = (adiabatic_raised_pressure / initial_temperature) * raised_temperature
-# Bore of engine = 99.5mm, stroke of engine = 79mm
+isochoric_raised_pressure = adiabatic_raised_pressure * (raised_temperature / initial_temperature)
+adiabatic_constant_3_4 = isochoric_raised_pressure * compressed_volume ** γ
+adiabatic_lowered_pressure = calculate_pressure(open_volume, adiabatic_constant_3_4, isochoric_raised_pressure)
+
+
 volume_range = np.linspace(compressed_volume, open_volume)
 volumes = [volume_range,  # 0->1; [V_min, V_max]
            volume_range,  # 1->2; [V_min, V_max]
@@ -89,25 +95,26 @@ volumes = [volume_range,  # 0->1; [V_min, V_max]
 
 # P_2 to P_3
 # V_min to V_max at P_0
-
+'''
+I don't know what it is
+'''
 pressures = [np.full(len(volume_range), initial_pressure),                                              # P = P_0
              calculate_pressure(volume_range, adiabatic_constant_1_2, initial_pressure),                # P = k_12 / V^γ
              np.linspace(adiabatic_raised_pressure, isochoric_raised_pressure, num=len(volume_range)),  # P = P2 (T2/T1)
-             calculate_pressure(volume_range, adiabatic_constant_3_4, adiabatic_raised_pressure),        # P = k_34 / V^γ
+             calculate_pressure(volume_range, adiabatic_constant_3_4, isochoric_raised_pressure),        # P = k_34 / V^γ
              np.linspace(adiabatic_lowered_pressure, initial_pressure, num=len(volume_range))
              ]
-for volume, pressure in zip(volumes, pressures):
-    plt.plot(volume, pressure)
+
+
+for branch_index, (volume, pressure) in enumerate(zip(volumes, pressures)):
+    plt.plot(volume, pressure, label=f"{branch_index}")
 
 # pressures_1 = np.full(len(Pressure_Range), Volume_2)
 # plt.plot(Pressure_Range, pressures_1)
 
-# plt.plot(Volume_Range, P(Volume_Range, adiabatic_constant_1_2, compressed_volume))
-# plt.xlabel('volume (m**3)')
-# plt.ylabel('pressure (pascal)')
-
-
-# plt.plot(np.full(len(compressed_volume, )
-
-
+#efficency
+net_work=np.trapz(pressures[3], volume_range) - np.trapz(pressures[1], volume_range)
+efficency= net_work/heat_in
+print(efficency)
+plt.legend()
 plt.show()
