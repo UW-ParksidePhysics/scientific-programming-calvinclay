82987f7eaad69a28a4a08c383359bb3a44e6b6fe
calvinclay2000 <124001562+calvinclay2000@users.noreply.github.com>
2023-05-08 16:33:04 -0500

    Create annotate_plot.py

---
+++ python/annotate_plot.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def annotate_plot(annotations):
+    """
+    Annotate a plot using Pyplot's text function
+    :param annotations: dict
+        Dictionary whose keys are the labels (strings) to be annotated and
+        values are dictionary with the following key-value pairs:
+        :'string': str
+            Text string for the text function
+        :'position': ndarray, shape (2)
+            x, y coordinates for the position of the textbox
+        :'alignment': list, str, shape (2)
+            horizontalalignment and verticalalignment values for the text
+            function
+        :'fontsize': float
+            Value of the font size in pixels
+    :return:
+    """
+    plt.txt(annotations['postion'][0],annotations['position'][1],annotations['string'],
+            horizontalalignment= annotations['alignment'][0], verticalalignment=annotations['alignment'][1],
+            fontsize=annotations['fontsize'])
+
+    return
+
+
+if __name__ == "__main__":
+    from datetime import date
+    import numpy as np
+    name = 'Michael Hotchkiss'
+    annotations = {'string': f"Created by {name} {date.today().isoformat()}",
+                   'position': np.array([4.75, 9.5]), 'alignment': ['left', 'bottom'], 'fontsize': 10}
+
+    plt.plot(5, 10)
+    plt.text(x=annotations['position'][0], y=annotations['position'][1], s=annotations['string'],
+             horizontalalignment=annotations['alignment'][0], verticalalignment=annotations['alignment'][1],
+             fontsize=annotations['fontsize'])
+    plt.show()
