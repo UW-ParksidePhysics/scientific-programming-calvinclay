22e8bd0642bbe92104d6b15142b89199bf29aa34
calvinclay2000 <124001562+calvinclay2000@users.noreply.github.com>
2023-05-08 16:26:44 -0500

    Update calculate_quadratic_fit.py

--- python/calculate_quadratic_fit.py
+++ python/calculate_quadratic_fit.py
@@ -1,11 +1,16 @@
+"""
+Finds quadratic coefficients
+"""
+
+__author__ = 'Michael Hotchkiss'
 import numpy as np
 
 
 def calculate_quadratic_fit(data):
     """
-
-    :param data:
-    :return:
+    This finds quadratic coefficients for a set of data points.
+    :param data: ndarray, shape(2,M)
+    :return: quadratic_coefficients: ndarray, shape(3,)
     """
     quadratic_coefficients = np.polyfit(data[0], data[1], 2)
     ordered_quadratic_coefficients = [quadratic_coefficients[-1], quadratic_coefficients[1], quadratic_coefficients[0]]
