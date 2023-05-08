cb3a8bfdfbdb072a39c89a9379f7a37ab0b921d1
calvinclay2000 <124001562+calvinclay2000@users.noreply.github.com>
2023-05-08 16:22:50 -0500

    Update calculate_bivariate_statistics.py

--- python/calculate_bivariate_statistics.py
+++ python/calculate_bivariate_statistics.py
@@ -1,3 +1,9 @@
+"""
+Return statistics on a set of data.
+"""
+
+__author__ = 'Michael Hotchkiss'
+
 import numpy as np
 from scipy import stats
 
@@ -8,10 +14,12 @@ x_values = np.linspace(-10,10,22)
 y_values = x_values**2
 def calculate_bivariate_statistics(data):
     """
+    Reads data and returns from text file and returns statistics
     calculating statistics of the volumes_energies.dat
-    :param data:
-    :return: statistics: ndarray, shape(6)
+    :param data: ndarray, shape(2,M)
+    :return: statisitics: ndarray, shape(6,)
     """
+
     try:
         statistics_x = stats.describe(data[0])
         statistics_y = stats.describe(data[1])
@@ -26,7 +34,7 @@ if __name__ == "__main__":
     y_values = x_values ** 2
     value_names = ['mean of y', 'std of y', 'min of x', 'max of x', 'min of y', 'max of y']
     expected_values = [np.mean(y_values), np.sqrt(np.var(y_values)), np.min(x_values), np.max(x_values), np.min(y_values), np.max(y_values)]
+    print('name:expected:value')
     for name, expected, value in zip(value_names, expected_values, calculate_bivariate_statistics([x_values,y_values])):
         print(f'{name}: {expected}, {value}')
     #print(calculate_bivariate_statistics(x_values,y_values))
-
